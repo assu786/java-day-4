8e96e15b6bd96823ae6631d00c4e4a9629483a86
assu786 <shaikaqsa.78@gmail.com>
2024-12-05 14:02:45 +0530

    Add files via upload

---
+++ divisornum.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class K
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int i=0;i<t;i++)
+        {
+            int n=sc.nextInt();
+            int c=0;
+            for(int j=1;j<=n;j++)
+            {
+                if((n%j)==0)
+                {
+                    c+=1;
+                }
+            }
+            System.out.println(c);
+        }
+    }
+}
