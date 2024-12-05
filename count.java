3edfff6acb650af035b5af6c3b7758bd5a95e79e
assu786 <shaikaqsa.78@gmail.com>
2024-12-05 12:39:48 +0530

    Add files via upload

---
+++ count.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class G
+{
+    public static void main(String args[])
+    {
+        Scanner m=new Scanner(System.in);
+        int t=m.nextInt();
+        for(int i=2;i<=t;i=i*2)
+        {
+            System.out.println(i);
+        }
+    }
+}
